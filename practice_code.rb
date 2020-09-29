1909a44709104354fdc79feb0ca859e93f935eed
Maximilian Rice <maximilian.rice1@gmail.com>
2020-09-29 17:18:20 +0000

    Automatically backed up by Learn

--- practice_code.rb
+++ practice_code.rb
@@ -1,6 +1,4 @@
 # I am trying to pracrie using an array
-team_one = Lakers
-team_two = Nuggets
-team_three = Celtics
-team_four = Heat
+
 nba_teams = ["Lakers" , "Nuggets" , "Celtics" , "Heat"]
+phrase = "hello"
